2cb25995f87ac92ffb3840adfd931b0b0086aaa0
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 17:31:27 +0530

    Add src/models/Post.js

---
+++ src/models/Post.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    name: { type: String }, // User's name at the time of comment
+    profilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
